2d680c64dbeb099ad4ebb5f6ef79e134538031ad
thebkbuffalo <evan.m.berg@gmail.com>
2014-10-01 14:09:17 -0400

    sessioning/log in and out works, but no password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    session[:user_id] = user.id
+    redirect_to(user_path(user))
+  end
+
+  def destroy
+    #actually do the log out information here
+    #and then redirect to whever we go post logout
+    session[:user_id] = nil
+    redirect_to(login_path)
   end
 end
